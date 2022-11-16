32e507404a335c6a98f6e1ed5c7edf6dc7ea1fbe
Phan Luan <phanluandev@gmail.com>
2022-11-16 11:02:01 +0700

    new update

--- view/detail.php
+++ view/detail.php
@@ -5,125 +5,146 @@
 <!------ Include the above in your HEAD tag ---------->
 
 <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800&display=swap" rel="stylesheet">
-  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
-  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
-  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css"><div class="pd-wrap">
-    <link rel="stylesheet" href="./view/assets/css/detail_product.css">
-    <div class="container">
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
+<div class="pd-wrap">
+  <link rel="stylesheet" href="./view/assets/css/detail_product.css">
+  <div class="container">
 
     <div class="breadcrumb-banner d-flex flex-wrap align-items-center justify-content-end">
-                <div class="col-first">
-                    <br>
-                    <nav class="d-flex align-items-center">
-                        <a href="index.php">Trang chủ<span class="lnr lnr-arrow-right"></span></a>
-                        <a href="index.php?act=detail">Chi tiết sản phẩm</a>
-                    </nav>
-                </div>
-            </div>
+      <div class="col-first">
+        <br>
+        <nav class="d-flex align-items-center">
+          <a href="index.php">Trang chủ<span class="lnr lnr-arrow-right"></span></a>
+          <a href="index.php?act=detail">Chi tiết sản phẩm</a>
+        </nav>
+      </div>
+    </div>
 
-          <div class="heading-section">
-              <h2>Product Details</h2>
+    <div class="heading-section">
+      <h2>Product Details</h2>
+    </div>
+    <div class="row">
+      <div class="col-md-6">
+        <div id="slider" class="owl-carousel product-slider">
+
+          <div class="item">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQI6nUmObt62eDkqNSmIvCN_KkQExtbpJmUbVx_eTh_Y3v3r-Jw" />
           </div>
+
+        </div>
+
+      </div>
+      <div class="col-md-6">
+        <div class="product-dtl">
+          <div class="product-info">
+            <div class="product-name">Variable Product</div>
+            <div class="reviews-counter">
+              <div class="rate">
+                <input type="radio" id="star5" name="rate" value="5" checked />
+                <label for="star5" title="text">5 stars</label>
+                <input type="radio" id="star4" name="rate" value="4" checked />
+                <label for="star4" title="text">4 stars</label>
+                <input type="radio" id="star3" name="rate" value="3" checked />
+                <label for="star3" title="text">3 stars</label>
+                <input type="radio" id="star2" name="rate" value="2" />
+                <label for="star2" title="text">2 stars</label>
+                <input type="radio" id="star1" name="rate" value="1" />
+                <label for="star1" title="text">1 star</label>
+              </div>
+              <span>3 Reviews</span>
+            </div>
+            <div class="product-price-discount"><span>$39.00</span></div>
+          </div>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
           <div class="row">
             <div class="col-md-6">
-              <div id="slider" class="owl-carousel product-slider">
-
-            <div class="item">
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQI6nUmObt62eDkqNSmIvCN_KkQExtbpJmUbVx_eTh_Y3v3r-Jw"/>
-            </div>
-           
-          </div>
-    
-            </div>
-            <div class="col-md-6">
-              <div class="product-dtl">
-                <div class="product-info">
-                  <div class="product-name">Variable Product</div>
-                  <div class="reviews-counter">
-                <div class="rate">
-                    <input type="radio" id="star5" name="rate" value="5" checked />
-                    <label for="star5" title="text">5 stars</label>
-                    <input type="radio" id="star4" name="rate" value="4" checked />
-                    <label for="star4" title="text">4 stars</label>
-                    <input type="radio" id="star3" name="rate" value="3" checked />
-                    <label for="star3" title="text">3 stars</label>
-                    <input type="radio" id="star2" name="rate" value="2" />
-                    <label for="star2" title="text">2 stars</label>
-                    <input type="radio" id="star1" name="rate" value="1" />
-                    <label for="star1" title="text">1 star</label>
-                  </div>
-                <span>3 Reviews</span>
-              </div>
-                  <div class="product-price-discount"><span>$39.00</span><span class="line-through">$29.00</span></div>
-                </div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                <div class="row">
-                  <div class="col-md-6">
-                    <label for="size">Size</label>
-                <select id="size" name="size" class="form-control">
-                  <option>S</option>
-                  <option>M</option>
-                  <option>L</option>
-                  <option>XL</option>
-                </select>
-                  </div>
-                  <div class="col-md-6">
-                    <label for="color">Color</label>
-                <select id="color" name="color" class="form-control">
-                  <option>Blue</option>
-                  <option>Green</option>
-                  <option>Red</option>
-                </select>
-                  </div>
-                </div>
-                <div class="product-count">
-                  <label for="size">Quantity</label>
-                  <form action="#" class="display-flex">
-                  <div class="qtyminus">-</div>
-                  <input type="text" name="quantity" value="1" class="qty">
-                  <div class="qtyplus">+</div>
-              </form>
-              <br>
-              <div class="checkout_btn_inner d-flex align-items-center">
-                    <a class="btn primary-btn" href="">Add to cart</a>
-                    </div>
-                </div>
-              </div>
+              <label for="size">Size</label>
+              <select id="size" name="size" class="form-control">
+                <option>S</option>
+                <option>M</option>
+                <option>L</option>
+                <option>XL</option>
+              </select>
             </div>
           </div>
-          <div class="product-info-tabs">
-            <ul class="nav nav-tabs" id="myTab" role="tablist">
-            <li class="nav-item">
-              <a class="nav-link active" id="description-tab" data-toggle="tab" href="#description" role="tab" aria-controls="description" aria-selected="true">Description</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" id="review-tab" data-toggle="tab" href="#review" role="tab" aria-controls="review" aria-selected="false">Reviews (0)</a>
-            </li>
-        </ul>
-        <div class="tab-content" id="myTabContent">
-            <div class="tab-pane fade show active" id="description" role="tabpanel" aria-labelledby="description-tab">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.
+          <div class="product-count">
+            <label for="size">Quantity</label>
+            <form action="#" class="display-flex">
+              <div class="qtyminus">-</div>
+              <input type="text" name="quantity" value="1" class="qty">
+              <div class="qtyplus">+</div>
+            </form>
+            <br>
+            <div class="checkout_btn_inner d-flex align-items-center">
+              <a class="btn primary-btn" href="">Add to cart</a>
             </div>
-            <div class="tab-pane fade" id="review" role="tabpanel" aria-labelledby="review-tab">
-              <div class="review-heading">REVIEWS</div>
-              <p class="mb-20">There are no reviews yet.</p>
-              <form class="review-form">
-                 
-                  <div class="form-group">
-                    <label>Your message</label>
-                    <textarea class="form-control" rows="10"></textarea>
-                  </div>
-                  </div>
-                  <div class="checkout_btn_inner d-flex align-items-center">
-                    <a class="btn primary-btn" href="">Submit Review</a>
-                    </div>
-                </form>
-            </div>
+          </div>
         </div>
       </div>
-      
-      
+    </div>
+
+    <div class="product-info-tabs">
+      <ul class="nav nav-tabs" id="myTab" role="tablist">
+        <li class="nav-item">
+          <a class="nav-link active" id="description-tab" data-toggle="tab" href="#description" role="tab" aria-controls="description" aria-selected="true">Description</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" id="review-tab" data-toggle="tab" href="#review" role="tab" aria-controls="review" aria-selected="false">Reviews (0)</a>
+        </li>
+      </ul>
+      <div class="tab-content" id="myTabContent">
+        <div class="tab-pane fade show active" id="description" role="tabpanel" aria-labelledby="description-tab">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.
+        </div>
+        <div class="tab-pane fade" id="review" role="tabpanel" aria-labelledby="review-tab">
+          <div class="review-heading">REVIEWS</div>
+          <p class="mb-20">There are no reviews yet.</p>
+          <form class="review-form">
+
+            <div class="form-group">
+              <label>Your message</label>
+              <textarea class="form-control" rows="10"></textarea>
+            </div>
+        </div>
+        <div class="checkout_btn_inner d-flex align-items-center">
+          <a class="btn primary-btn" href="">Submit Review</a>
+        </div>
+        </form>
+      </div>
+    </div>
+    <!-- single product slide -->
+    <div class="single-product-slider">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-lg-6 text-center mt-5">
+            <div class="section-title mt-5">
+              <h1>Sản phẩm cùng loại</h1>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <!-- single product -->
+          <div class="col-lg-3 col-md-6">
+            <div class="single-product">
+              <img class="img-fluid" src="./view/assets/img/product/p1.jpg" alt="">
+              <div class="product-details">
+                <h6>addidas New Hammer sole
+                  for Sports person</h6>
+                <div class="price">
+                  <h6>$150.00</h6>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity=" sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+
+</div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity=" sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
